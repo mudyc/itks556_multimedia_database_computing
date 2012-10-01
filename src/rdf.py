3b8acf88f3047fdc7de67752ae12995e4053741b
Matti Katila <matti.katila@gmail.com>
2012-10-01 21:32:24 +0300

    Implement locking to prevent datafile issues again.

--- src/rdf.py
+++ src/rdf.py
@@ -1,6 +1,6 @@
 # simple flat file rdf database
 
-import logging
+import logging, os, time
 
 _rdf = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
 _xsd = 'http://www.w3.org/2001/XMLSchema#'
@@ -10,9 +10,20 @@ lit_float = _xsd + 'float'
 
 def aqcuire_lock(f):
     """http://rcrowley.org/2010/01/06/things-unix-can-do-atomically.html"""
-    pass
-def release_lock():
-    pass
+    #logging.info("aqcuire")
+    t = 0.002
+    while True:
+        try:
+            os.link(f, f+'.lock')
+            #logging.info("aqcuired")
+            break
+        except:
+            time.sleep(t)
+            if t < 1.0:
+                t *= 2
+
+def release_lock(f):
+    os.unlink(f+'.lock')
 
 def rm(file_, subj, pred, obj):
     aqcuire_lock(file_)
@@ -24,11 +35,13 @@ def rm(file_, subj, pred, obj):
     test = subj +' '+pred+' '+obj+'\n'
     lines.remove(test)
 
-    f = open(file_, 'w')
+    new = os.tempnam(os.getcwd())
+    f = open(new, 'w')
     f.writelines(lines)
     f.close()
+    os.rename(new, file_)
     
-    release_lock()
+    release_lock(file_)
 
 def add(file_, subj, pred, obj):
     aqcuire_lock(file_)
@@ -42,11 +55,14 @@ def add(file_, subj, pred, obj):
     lines.sort()
     #logging.info(lines)
 
-    f = open(file_, 'w')
+    new = os.tempnam(os.getcwd())
+    #logging.info(new)
+    f = open(new, 'w')
     f.writelines(lines)
     f.close()
+    os.rename(new, file_)
     
-    release_lock()
+    release_lock(file_)
 
 def has(file_, subj, pred, obj):
     f = open(file_, 'r')
